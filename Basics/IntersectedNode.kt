3f0a63fb6ef662cdd94b75a764569025a4d2e98f
rebelbob11 <rebelbob11@gmail.com>
2025-06-30 00:36:55 -0700

    Two pointer technique added to intersected  node finding

--- Basics/IntersectedNode.kt
+++ Basics/IntersectedNode.kt
@@ -14,22 +14,40 @@
 
 class Solution {
     fun getIntersectionNode(headA:ListNode?, headB:ListNode?):ListNode? {
-        var hashSet: HashSet<ListNode?> = hashSetOf()
-        var current: ListNode? = headA
 
-        while (current != null) {
-            hashSet.add(current)
-            current = current?.next
+        //Hashmap technique
+        //Time complexity O(m+n)
+        //Space complexity O(m+n)
+//        var hashSet: HashSet<ListNode?> = hashSetOf()
+//        var current: ListNode? = headA
+//
+//        while (current != null) {
+//            hashSet.add(current)
+//            current = current?.next
+//        }
+//
+//        current = headB
+//        while (current != null) {
+//            if (hashSet.contains(current)) {
+//                return current
+//            }
+//            hashSet.add(current)
+//            current = current?.next
+//        }
+//        return null
+
+        //Two pointer technique
+        //Time complexity O(m+n)
+        //Space complexity O(1)
+
+        var pointer1: ListNode? = headA
+        var pointer2:ListNode? = headB
+
+        while(pointer1 != pointer2) {
+            pointer1 = if(pointer1 == null) headB else pointer1.next
+            pointer2 = if(pointer2 == null) headA else pointer2.next
         }
 
-        current = headB
-        while (current != null) {
-            if (hashSet.contains(current)) {
-                return current
-            }
-            hashSet.add(current)
-            current = current?.next
-        }
-        return null
+        return pointer1
     }
 }
